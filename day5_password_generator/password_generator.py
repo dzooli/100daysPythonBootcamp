3320289b80d3ade22a9ac72d54bd16ddfec40d3d
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-01-12 23:04:22 +0100

    Day6: Maze solver for Reeborg + some linting

--- day5_password_generator/password_generator.py
+++ day5_password_generator/password_generator.py
@@ -1,7 +1,7 @@
 import random
 
-symbols = "! % / @ # ( ) _ $ < >"
-chars = "A B C D E F G H I J K L M N O P Q U R S T X Y Z W V"
+SYMBOLS = "! % / @ # ( ) _ $ < >"
+CHARS = "A B C D E F G H I J K L M N O P Q U R S T X Y Z W V"
 
 pw_len = int(input("How many characters would you like in your password?\n"))
 symbol_count = int(input("How many symbols would you like?\n"))
@@ -10,7 +10,7 @@ char_count: int = pw_len - symbol_count - number_count
 
 generated_passwd = []
 for char in range(0, char_count):
-    generated_passwd.append(str(random.choice(chars.split())))
+    generated_passwd.append(str(random.choice(CHARS.split())))
 for index_to_lowercase in range(0, char_count):
     random_lowercase = random.randint(0, 1000)
     if random_lowercase % 2 == 0:
@@ -19,9 +19,9 @@ for index_to_lowercase in range(0, char_count):
 for char in range(0, number_count):
     generated_passwd.append(str(random.randint(0, 9)))
 for char in range(0, symbol_count):
-    generated_passwd.append(str(random.choice(symbols.split())))
+    generated_passwd.append(str(random.choice(SYMBOLS.split())))
 
 random.shuffle(generated_passwd)
 # List to string
-final_password = ''.join(str(elem) for elem in generated_passwd)
+final_password: str = ''.join(str(elem) for elem in generated_passwd)
 print(F"Your generated password is: {final_password}")
